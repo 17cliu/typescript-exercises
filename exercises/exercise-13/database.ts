f08f51a56281f6df8643e451f92960884a259fc6
Caroline Liu <17cliu@users.noreply.github.com>
2021-04-02 14:53:51 -0400

    Set up types for query object for Exercise 13

--- exercises/exercise-13/database.ts
+++ exercises/exercise-13/database.ts
@@ -1,13 +1,35 @@
+interface PropertyFilter {
+    $eq?: string | number;
+    $gt?: string | number;
+    $lt?: string | number;
+    $in?: string[] | number[];
+}
+
+interface PropertyQuery {
+    [k: string]: PropertyFilter;
+}
+
+interface MultiplePropertiesQuery {
+    $and?: PropertyQuery[];
+    $or?: PropertyQuery[];
+}
+
+interface TextSearchQuery {
+    $text: string;
+}
+
+type Query = PropertyQuery | TextSearchQuery | MultiplePropertiesQuery;
+
 export class Database<T> {
     protected filename: string;
     protected fullTextSearchFieldNames: unknown[];
 
-    constructor(filename: string, fullTextSearchFieldNames) {
+    constructor(filename: string, fullTextSearchFieldNames: string[]) {
         this.filename = filename;
         this.fullTextSearchFieldNames = fullTextSearchFieldNames;
     }
 
-    async find(query): Promise<T[]> {
+    async find(query: Query): Promise<T[]> {
         return [];
     }
 }
