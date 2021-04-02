441f58e4f2d768595c8e3f28fdd3f98a791daf45
Caroline Liu <17cliu@users.noreply.github.com>
2021-04-02 12:17:13 -0400

    Complete Exercise 12

--- exercises/exercise-12/module-augmentations/date-wizard/index.ts
+++ exercises/exercise-12/module-augmentations/date-wizard/index.ts
@@ -3,4 +3,18 @@ import 'date-wizard';
 
 declare module 'date-wizard' {
     // Add your module extensions here.
+
+    // Notice that I only have to define the properties that are
+    // missing from the official type! The declarations will be
+    // merged together, so I don't need to repeat things.
+    interface DateDetails {
+        hours: number;
+        minutes: number;
+        seconds: number;
+    }
+
+    // Same here. I don't have to redefine the functions that have
+    // already been correctly typed. I just have to add the additional
+    // functions that were missing in the existing module definition.
+    function pad(s: string | number): string;
 }
